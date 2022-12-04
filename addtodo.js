d6917a990615dec607d24534c18c86e23e266a2d
Sarthak Yelne <sarthakyelne2003@gmail.com>
2022-12-04 21:10:55 +0530

    Added

--- addtodo.js
+++ addtodo.js
@@ -20,7 +20,7 @@ const getJSDate = (days) => {
 };
 (async () => {
   const { title, dueInDays } = argv;
-  if (!title || dueInDays === undefined) {
+  if (!title || !dueInDays) {
     throw new Error(
       'title and dueInDays are required. \nSample command: node addTodo.js --title="Buy milk" --dueInDays=-2 '
     );
